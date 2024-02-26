ef0349d0bf627389ee95ef7e1572d03f43990255
Shane Poppleton <shane@bjja.com.au>
2024-02-26 12:20:56 +1000

    Improve the script to force running under non-root user

---
+++ README.md
@@ -0,0 +1,13 @@
+# Forge server setup
+
+## Add details here to get server setup
+install ubuntu, provision with forge
+
+## Run the setup script to setup your Forge Server defaults
+
+Run the following command to setup your instance to my preferences.
+
+```bash
+curl -sSL https://raw.githubusercontent.com/Alpha-IT-Centre/forge-setup/setup.sh | sh
+```
+
--- setup.sh
+++ setup.sh
@@ -2,9 +2,18 @@
 
 # Ensure the script is run as root
 if [[ $EUID -ne 0 ]]; then
-	echo "This script must be run as root"
+	echo "This script must be run with sudo, but not as a logged-in root user."
 	exit 1
 fi
+
+# Check if the logged-in user is root (meaning the script wasn't invoked with sudo from a non-root user)
+if [[ -z $SUDO_USER || $SUDO_USER = "root" ]]; then
+	echo "Please run this script using sudo from a non-root user."
+	exit 1
+fi
+
+# Install required packages
+apt-get install stow
 
 # Set root's home directory to /home/forge
 usermod -d /home/forge root
